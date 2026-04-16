8eb7556c4b07f9cd4e874949501b43a4e6a0a18a
Akash Chaudhary <144554996+akash15072004@users.noreply.github.com>
2026-04-17 04:47:28 +0530

    Change default port from 8000 to 8080

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import os
 
-port = int(os.environ.get("PORT", 8000))
+port = int(os.environ.get("PORT", 8080))
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
